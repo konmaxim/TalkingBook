ebb10cbb7d114c321a9c63d5e3ba8d8bd906273b
konmaxim <kontsevitchmaxim@gmail.com>
2025-09-21 03:00:42 +0300

    Fixed hallucinations

--- main.py
+++ main.py
@@ -13,6 +13,9 @@ from fastapi.responses import FileResponse
 from dotenv import load_dotenv
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_openai import ChatOpenAI 
+import shutil
+import chromadb
+from chromadb.config import Settings
 app = FastAPI()
 static_path = os.path.join(os.path.dirname(__file__), "static")
 print("Serving static files from:", static_path)
@@ -26,6 +29,7 @@ app.add_middleware(
     allow_headers=["*"],
 )
 load_dotenv()
+
 api_key = os.environ.get("OPENAI_API_KEY")
 CHROMA_PATH = "chroma"
 TEMPLATE = """
@@ -33,12 +37,11 @@ TEMPLATE = """
 Контекст:
 {context}
 
-Требования:
-- Отвечай исключительно в стиле, манере и языке данного контекста.  
-- Не добавляй собственных объяснений или информации, которой нет в контексте.  
+Отвечай на следующий вопрос исходя из контекста, приведённого выше
 
 Вопрос: {question}
 """
+COLLECTION_NAME = "current_doc"
 def get_file_type(filepath: str) -> str:
     ext = os.path.splitext(filepath)[1].lower()
     return ext
@@ -53,13 +56,20 @@ def load_document(filepath: str):
         return UnstructuredWordDocumentLoader(filepath).load()
     else:
         raise ValueError(f"Документ должен быть в формате pdf/txt/doc/docx: {ext}")
-def split_docs(docs, chunk_size=300, chunk_overlap=50):
+def split_docs(docs, chunk_size=500, chunk_overlap=50):
     splitter = RecursiveCharacterTextSplitter(
         chunk_size=chunk_size,
         chunk_overlap=chunk_overlap
     )
     chunks = splitter.split_documents(docs)
     return chunks
+def reset_collection():
+    client = chromadb.Client(Settings(persist_directory=CHROMA_PATH))
+    try:
+        client.delete_collection(COLLECTION_NAME)
+        print(f"Collection '{COLLECTION_NAME}' deleted.")
+    except chromadb.errors.NotFoundError:
+        print(f"(first upload).")
 @app.get("/")
 async def root():
     return FileResponse("static/index.html")
@@ -68,17 +78,17 @@ async def get_chat():
     return FileResponse("static/chat.html")
 @app.post("/upload")
 async def upload_file(file: UploadFile = File(...)):
-    print("one two")
+
     _, ext = os.path.splitext(file.filename)
     with NamedTemporaryFile(delete=False, suffix=ext) as tmp:
             tmp.write(await file.read())
             tmp_path = tmp.name
 
-       
+    reset_collection()   
     docs = load_document(tmp_path)
     chunks = split_docs(docs)
- 
-    db = Chroma.from_documents(chunks, OpenAIEmbeddings(model="text-embedding-3-small"), persist_directory=CHROMA_PATH)
+    db = None
+    db = Chroma.from_documents(chunks, OpenAIEmbeddings(model="text-embedding-3-small"), persist_directory=CHROMA_PATH,collection_name=COLLECTION_NAME)
     print ("Saved all chunks")
     return {"message": f"File '{file.filename}' uploaded successfully!"}
 
@@ -86,10 +96,8 @@ async def upload_file(file: UploadFile = File(...)):
 async def answer(request: Request):
     data = await request.json()
     question_text = data.get("question")
-    #база данных
-    vectordb = Chroma(persist_directory=CHROMA_PATH, embedding_function=OpenAIEmbeddings(model="text-embedding-3-small"))
-    #поиск 
-    fournn = vectordb.similarity_search_with_relevance_scores(question_text, k=4)
+    db = Chroma(persist_directory=CHROMA_PATH,collection_name=COLLECTION_NAME,embedding_function= OpenAIEmbeddings(model="text-embedding-3-small"))
+    fournn = db.similarity_search_with_relevance_scores(question_text, k=3)
     context_text = "\n\n --- \n\n".join([doc.page_content for doc, _score in fournn])
     prompt_template = ChatPromptTemplate.from_template(TEMPLATE)
     prompt = prompt_template.format(context=context_text, question = question_text)
